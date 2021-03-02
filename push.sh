e03de94f53687c01e55df1395cda9e6f619ec19b
KevinZhang <zhang_kun_lin@outlook.com>
2021-03-02 21:58:31 +0800

    add git ignore

---
+++ push.sh
@@ -0,0 +1,11 @@
+echo 'make dictionary ...'
+book sm
+
+echo 'copy summary.md to index.md ...'
+cp SUMMARY.md index.md
+
+echo 'git commit ...'
+git commit -a -m "add md file"
+
+echo 'git push ...'
+git push
